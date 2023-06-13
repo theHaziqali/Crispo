9b7023e9c6c6e45a958fdab4313c0315db5588c8
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2023-06-14 00:32:57 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Crispo
 Flask application with an api to access data from sql
+This readme file provides instructions and information about the Flask application that is designed to be accessed through root access using Python for Windows, pip for Windows, and XAMPP server. Please make sure you have the following prerequisites installed and set up before proceeding with the Flask application.
+
+## Prerequisites
+Python for Windows: Make sure you have Python installed on your Windows machine. You can download the latest version of Python for Windows from the official Python website (https://www.python.org/downloads/windows/). Follow the installation instructions provided by the Python installer.
+
+Pip for Windows: Pip is the package installer for Python. It is usually installed automatically with Python. To verify if pip is installed, open a command prompt and run the following command:
+
+If pip is not installed, you can download and install it from the official pip website (https://pip.pypa.io/en/stable/installing/).
+
+## XAMPP Server:
+ XAMPP is a popular Apache distribution that contains MySQL, PHP, and Perl. It provides a local server environment for developing and testing web applications. Make sure you have XAMPP server installed and running on your Windows machine. You can download XAMPP from the Apache Friends website (https://www.apachefriends.org/index.html) and follow the installation instructions provided.
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request,jsonify
 import pymysql
+from user import User
 
 app = Flask(__name__)
 
@@ -9,27 +10,51 @@ app = Flask(__name__)
 host = 'localhost'
 user = 'root'
 password = ''
-database = 'crispo'
+database = 'Crispo'
 
 @app.route('/')
-def index():
-    # Connect to MySQL
-    conn = pymysql.connect(host=host, user=user, password=password, database=database)
-    
-    # Create a cursor object
-    cursor = conn.cursor()
-    
-    # Execute a sample query
-    cursor.execute("SELECT * FROM mytable")
-    
-    # Fetch all rows from the result
-    rows = cursor.fetchall()
-    
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
-    
-    return render_template('index.html', rows=rows)
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    #json request to get data
+    data = request.get_json()
+
+    # Extracting data from the request
+    username = data['username']
+    email = data['email']
+    passwd = data['password']
+    # print(username)
+    # print(email)
+    # print(passwd)
+
+    try:
+        # Connect to MySQL
+        conn = pymysql.connect(host=host, user=user, password=password, database=database)
+        cursor = conn.cursor()
+
+        # Check if the username or email already exists in the user table
+        query = "SELECT * FROM user WHERE username = %s OR email = %s"
+        cursor.execute(query, (username, email))
+        result = cursor.fetchall()
+
+        if result:
+            return 'Username or email already exists.', 400
+
+        # Insert the user into the user table
+        query = "INSERT INTO user (username, email, password) VALUES (%s, %s, %s)"
+        cursor.execute(query, (username, email, passwd))
+        conn.commit()
+        #closing the connection
+        cursor.close()
+        conn.close()
+        new_user = User(username, email, password)
+
+        return 'User registered successfully.', 200
+    except pymysql.connector.Error as error:
+        return f"Error connecting to MySQL: {error}", 500
 
 if __name__ == '__main__':
     app.run()
